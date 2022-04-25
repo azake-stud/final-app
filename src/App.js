79c116517e3ca8313fe4d5b70c8e45251c92e6dc
azake1 <mr.rysbaev@gmail.com>
2022-04-25 23:53:19 +0600

    adding card parts

--- src/App.js
+++ src/App.js
@@ -4,15 +4,46 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h1 className='title'>Search Movies</h1>
-      <input placeholder='Movie name' className='search-item' type="" />
-      <p className='error-msg'>Error</p>
-
-      <div className='content'>
-        <div className='movie'>
-          
-        </div>
-
+      <h1 className="title">Search Movies</h1>
+      <input placeholder="Search smt" className="search-input" />
+      <p className="error-msg">Error</p>
+      <div className="content">
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
+        <a href="#" className="movie">
+          <div className="movie-detail">
+            <h3 className="movie-name">Spider man</h3>
+            <p className="movie-year">2021</p>
+          </div>
+        </a>
       </div>
     </div>
   );
