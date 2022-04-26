17640ee252f4ee35891a36122dd2e512acba7d2a
azake1 <mr.rysbaev@gmail.com>
2022-04-26 23:24:25 +0600

    editing after deploy to heroku

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { useFetch } from "../hooks/useFetch";
 
 export const HomePage =() =>{
     document.body.style.backgroundImage = "url('https://s.studiobinder.com/wp-content/uploads/2021/07/What-is-the-Studio-System-%E2%80%94-Hollywood-s-Studio-Era-Explained-Featured.jpg')";
-    const [keyword, setKeyword]= useState('world');
+    const [keyword, setKeyword]= useState('');
     let { movies, errorMsg } = useFetch(
       keyword !== "" ? `&s=${keyword}` : ""
     );
