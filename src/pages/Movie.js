068e0c3cad9deeb8738127179bbb351f6e676911
azake1 <mr.rysbaev@gmail.com>
2022-04-26 22:00:13 +0600

    fixed some bugs, but still no showing movie details

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,27 +1,24 @@
-import {useFetch} from '../hooks/useFetch';
-import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
-export const MoviePage =() =>{
-    let params = useParams();
-    const {movies} =useFetch(`&i=${params?.id}`);
+export const MoviePage = () => {
+  let params = useParams();
+  const { movies } = useFetch(`&i=${params?.id}`);
 
-
-    return (
-        <div className="container">
-            <div className="row">
-                <img className='bg-img' src={movies?.Poster} alt="" />
-                <div className="movie-data">
-                    <h1>{movies?.Title}</h1>
-                    <p className='movie-desc'>{movies?.Plot}</p>
-                    <p className='movie-year bold'>{movies?.Year}</p>
-                    <Link to="/">
-                     <button className="back-btn">Back to Movies</button>
-                     </Link>
-                </div>
-            </div>
-
+  return (
+    <div className="container">
+      <div className="row">
+        <img className="bg-img" src={movies?.Poster} />
+        <div className="movie-data">
+          <h1>{movies?.Title}</h1>
+          <p className="movie-desc">{movies?.Plot}</p>
+          <p className="movie-year bold">{movies?.Year}</p>
+          <Link to="/">
+            <button className="back-btn">Back to Movies</button>
+          </Link>
         </div>
+      </div>
 
-    )
+    </div>
+  );
 };
